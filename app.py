526be6d9b47605f91321fe4a611d5cebfb8fea46
Buhyeonuk <chillysaucing@gmail.com>
2022-03-29 01:02:09 +0900

    success extract jobkr pages!

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from jobkr import extract_jobkr_pages, extract_jobkr_jobs
 
 last_jobkr_page = extract_jobkr_pages()
+print(last_jobkr_page, '라스트페이지')
 
 jobkr_jobs = extract_jobkr_jobs(last_jobkr_page)
+
+print(jobkr_jobs)
--- jobkr.py
+++ jobkr.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-PERIOD = 3
+PERIOD = 4
 URL = f'https://www.jobkorea.co.kr/Search/?stext=%ED%94%84%EB%A1%A0%ED%8A%B8%EC%97%94%EB%93%9C&tabType=recruit&period={PERIOD}'
 
 
@@ -24,16 +24,30 @@ def extract_jobkr_pages():
     return max_page
 
 
+def extract_job(html):
+    title = html.find(
+        'div', class_='post-list-info').find('a', class_="title dev_view")
+    company = html.find(
+        'div', class_='post-list-corp').find('a', class_='name dev_view')
+    location = html.find('span', class_='loc long')
+    job_id = html['data-gno']
+    if title != None and company != None and location != None:
+        title = title['title']
+        company = company.string
+        location = location.string
+        return {'title': title, 'company': company, 'location': location, 'link': f'https://www.jobkorea.co.kr/Recruit/GI_Read/{job_id}'}
+
+
 def extract_jobkr_jobs(last_pages=0):
     jobs = []
-    # for page in range(last_pages):
-    result = requests.get(f'{URL}&Page_No={0 + 1}')
-    soup = BeautifulSoup(result.text, 'html.parser')
+    for page in range(last_pages):
+        # print(f'scrapping page {page+1}')
+        result = requests.get(f'{URL}&Page_No={page + 1}')
+        soup = BeautifulSoup(result.text, 'html.parser')
 
-    job_list = soup.find_all('div', class_='post')
-    for job in job_list:
-        job_name = job.find(
-            'div', class_='post-list-info').find('a', class_="title dev_view")
-        if job_name != None:
-            jobs.append(job_name['title'])
+        results = soup.find_all('li', class_='list-post')
+        for result in results:
+            job = extract_job(result)
+            if job != None:
+                jobs.append(job)
     return jobs
