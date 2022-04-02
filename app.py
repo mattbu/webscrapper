a7026b9567108112174b360f37a5480ae07a3879
Buhyeonuk <chillysaucing@gmail.com>
2022-04-02 15:58:20 +0900

    success scrapping so companies

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from jobkr import get_jobs as get_jobkr_jobs
 from so import get_jobs as get_so_jobs
 
-# jobkr_jobs = get_jobkr_jobs()
+jobkr_jobs = get_jobkr_jobs()
 so_jobs = get_so_jobs()
+jobs = jobkr_jobs + so_jobs
+
+print(jobs)
--- so.py
+++ so.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = f'https://stackoverflow.com/jobs?q=python'
+URL = f'https://stackoverflow.com/jobs/companies?q=python'
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36'}
 
@@ -14,27 +14,39 @@ def get_last_page():
     return int(last_page)
 
 
+def remove_special_text(title):
+    splited_title = title.split()
+    new_titles = []
+    for t in splited_title:
+        new_titles.append(''.join(filter(str.isalnum, t)))
+    return new_titles
+
+
 def extract_job(html):
-    # title = html.find('h2').find('a', class_='s-link stretched-link')
     title = html.find(
-        'h2', class_='mb4 fc-black-800 fs-body3')
-    company = html.find('h3', class_='fc-black-700 fs-body1 mb4')
-    print(company)
+        'h2', class_='fs-body2 mb4')
     if title != None:
-        title = title.find('a', class_='s-link stretched-link')['title']
-    return {'title': title}
+        title = title.find('a', class_='s-link').string
+    location, company = html.find(
+        'div', class_='d-flex gs12 gsx ff-row-wrap fs-body1').find_all('div')
+    location = location.get_text(strip=True)
+    company = company.get_text(strip=True)
+
+    removed_spcial_text_job_id = remove_special_text(title)
+    job_id = '-'.join(removed_spcial_text_job_id)
+    return {'title': title, 'location': location, 'company': company, 'link': f'https://stackoverflow.com/jobs/companies/{job_id}'}
 
 
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
+        # print(f'scrapping so: page: {page}')
         result = requests.get(f'{URL}&pg={page + 1}', headers=HEADERS)
         soup = BeautifulSoup(result.text, 'html.parser')
-        results = soup.find_all('div', class_='flex--item fl1')
+        results = soup.find_all('div', class_='flex--item fl1 text mb0')
         for result in results:
             job = extract_job(result)
             jobs.append(job)
-    # print(jobs)
     return jobs
 
 
